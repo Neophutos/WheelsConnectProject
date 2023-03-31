6f1b6d22437a1df97f0a017b364f0fb0a4d511ab
Ben Köppe <ben@petrolhead.de>
2023-03-31 10:21:18 +0200

    Init

---
+++ WheelsConnect/src/main/java/com/WheelsConnect/WheelsConnectApplication.java
@@ -0,0 +1,13 @@
+package com.WheelsConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelsConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelsConnectApplication.class, args);
+	}
+
+}
---
+++ WheelsConnect/src/test/java/com/WheelsConnect/WheelsConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WheelsConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelsConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
