ed4139c3d9e5ced007323fb34105768b1ad6a0d7
Ben Köppe <ben@petrolhead.de>
2023-05-19 11:22:28 +0200

    renewed Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Bar, Doughnut} from 'react-chartjs-2';
-import { Chart, DoughnutController, BarController, BarElement, ArcElement, LinearScale, CategoryScale } from 'chart.js';
+import { Chart, DoughnutController, BarController, BarElement, ArcElement, LinearScale, CategoryScale, Tooltip } from 'chart.js';
 
-Chart.register(DoughnutController, BarController, BarElement, ArcElement, LinearScale, CategoryScale);
-
+Chart.register(DoughnutController, BarController, BarElement, ArcElement, LinearScale, CategoryScale, Tooltip);
 
 const Dashboard = () => {
     const [kundenAnzahl, setKundenAnzahl] = useState(0);
@@ -46,14 +45,6 @@ const Dashboard = () => {
         return acc;
     }, []);
 
-
-    const beliebtesteFahrzeuge = fahrzeuge.map((fahrzeug) => ({
-        name: fahrzeug.name,
-        bookings: buchungen.filter(
-            (buchung) => buchung.fahrzeug.id === fahrzeug.id
-        ).length,
-    }));
-
     const createDoughnutCharts = (standorte) => {
         return standorte.map((standort, index) => {
             const { count, kapazitaet } = standort;
@@ -67,22 +58,51 @@ const Dashboard = () => {
                 ],
             };
 
+            const options = {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                var label = context.label || '';
+                                if (label === 'Fahrzeuge') {
+                                    label += ': ' + count;
+                                } else if (label === 'Freie Plätze') {
+                                    label += ': ' + (kapazitaet - count);
+                                }
+                                return label;
+                            }
+                        }
+                    }
+                }
+            }
+
             return (
                 <div key={index} style={{ maxWidth: "250px", maxHeight: "250px", padding: "20px", marginBottom: "50px"}}>
                     <h4>{standort.standort}</h4>
-                    <Doughnut data={data} width={150} height={150} />
+                    <Doughnut data={data} options={options} width={150} height={150} />
                 </div>
             );
-
         });
     };
+
+
+
+    // beliebtesteFahrzeuge sortiert nach Buchungsanzahl
+    const beliebtesteFahrzeuge = fahrzeuge
+        .map((fahrzeug) => ({
+            name: fahrzeug.marke + ' ' + fahrzeug.modell,
+            buchungen: buchungen.filter(
+                (buchung) => buchung.fahrzeug.id === fahrzeug.id
+            ).length,
+        }))
+        .sort((a, b) => b.buchungen - a.buchungen);
 
     const barData = {
         labels: beliebtesteFahrzeuge.map((fahrzeug) => fahrzeug.name),
         datasets: [
             {
                 label: 'Anzahl der Buchungen',
-                data: beliebtesteFahrzeuge.map((fahrzeug) => fahrzeug.bookings),
+                data: beliebtesteFahrzeuge.map((fahrzeug) => fahrzeug.buchungen),
                 backgroundColor: 'rgba(75, 192, 192, 0.6)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1,
@@ -90,26 +110,23 @@ const Dashboard = () => {
         ],
     };
 
+
     return (
         <div>
-            <h2>Dashboard</h2>
+            <h2 style={{padding: 25}}><b>Willkommen bei WheelsConnect!</b></h2>
             <div>
-                <h3>Anzahl der Kunden</h3>
-                <div>{kundenAnzahl}</div>
+                <h5 style={{padding: 10}}>Aktuell sind {kundenAnzahl} Kunden bei WheelsConnect registriert.</h5>
+                <h5 style={{padding: 10}}>Im aktuellen Monat sind {currentMonthBookings} Buchungen vorhanden.</h5>
             </div>
-            <div>
-                <h3>Anzahl der Buchungen im aktuellen Monat</h3>
-                <div>{currentMonthBookings}</div>
-            </div>
-            <div>
-                <h3>Standorte und die Anzahl der Fahrzeuge</h3>
+            <div style={{padding: 25}}>
+                <h4><b>Standorte und die Anzahl der Fahrzeuge</b></h4>
                 <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
                     {createDoughnutCharts(fahrzeugStandorte)}
                 </div>
             </div>
-            <div>
-                <h3>Beliebteste Fahrzeuge</h3>
-                <Bar data={barData} options={{ scales: { y: { beginAtZero: true } } }} />
+            <div style={{padding: 10}}>
+                <h4><b>Die am häufigsten gebuchten Fahrzeuge</b></h4>
+                <Bar data={barData} />
             </div>
         </div>
     );
